b766d296d386944570e135ffad843cddd59b4a6b
Song <w2033@e-mirim.hs.kr>
2021-07-20 09:33:10 +0900

    Complete ResultActivity.java Commit

--- app/src/main/java/kr/hs/emirim/w2033/project10_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2033/project10_2/MainActivity.java
@@ -2,8 +2,10 @@ package kr.hs.emirim.w2033.project10_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -30,5 +32,16 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+        Button btnFinish = findViewById(R.id.btn_done);
+        btnFinish.setOnClickListener(btnListener);
     }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(),ResultActivity.class);
+            intent.putExtra("voteCount",voteCount);
+            intent.putExtra("imgNames",imgNames);
+            startActivity(intent);
+        }
+    };
 }
